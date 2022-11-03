c3923315da2619bbde93564a268f65080fc2801f
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-11-03 20:10:08 -0300

    ✨ feat(backend): create endpoints
    - /guesses/count
    - /users/count

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
+import { z, z as zod } from 'zod'
 import { PrismaClient } from '@prisma/client'
+import ShortUniqueId from 'short-unique-id'
 
 const prisma = new PrismaClient({
   log: ['query'],
@@ -22,7 +24,39 @@ async function bootstrap() {
     return { count }
   })
 
-  await fastify.listen({ port: 3333, host: '0.0.0.0' })
+  fastify.get('/users/count', async () => {
+    const count = await prisma.user.count()
+
+    return { count }
+  })
+
+  fastify.get('/guesses/count', async () => {
+    const count = await prisma.guess.count()
+
+    return { count }
+  })
+
+  fastify.post('/pools', async (request, reply) => {
+    const createPoolBody = zod.object({
+      title: zod.string(),
+    })
+
+    const { title } = createPoolBody.parse(request.body)
+
+    const generatedId = new ShortUniqueId({ length: 6 })
+    const code = String(generatedId()).toUpperCase()
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code
+      }
+    })
+
+    return reply.status(201).send({ code })
+  })
+
+  await fastify.listen({ port: 3333/*, host: '0.0.0.0'*/ })
 }
 
 bootstrap()
