ec7fcc4ea84deade8312c598ca954c9ac337df37
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-11-06 02:57:19 -0300

    ✨ feat(backend): create users endpoint

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,4 +1,53 @@
 import { FastifyInstance } from "fastify";
 import { prisma } from "../lib/prisma";
+import { z as zod } from "zod";
 
-export async function authRoutes(fastify: FastifyInstance) {}
+export async function authRoutes(fastify: FastifyInstance) {
+  fastify.post("/users", async (request, response) => {
+    const createUserBody = zod.object({
+      access_token: zod.string(),
+    });
+
+    const { access_token } = createUserBody.parse(request.body);
+
+    const userResponse = await fetch(
+      "https://www.googleapis.com/oauth2/v2/userinfo",
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      }
+    );
+
+    const userData = await userResponse.json();
+
+    const userInfoSchema = zod.object({
+      id: zod.string(),
+      email: zod.string().email(),
+      name: zod.string(),
+      picture: zod.string().url(),
+    });
+
+    const userInfo = userInfoSchema.parse(userData);
+
+    let user = await prisma.user.findUnique({
+      where: {
+        googleId: userInfo.id,
+      },
+    });
+
+    if (!user) {
+      user = await prisma.user.create({
+        data: {
+          googleId: userInfo.id,
+          email: userInfo.email,
+          name: userInfo.name,
+          avatarUrl: userInfo.picture,
+        },
+      });
+    }
+
+    return { userInfo };
+  });
+}
